05f616bbb1fa61dd35e53ad9c5291b2bd0c94365
Carlos Guillén Viudez <carlos.guillen@col.vueling.com>
2020-07-11 17:25:14 +0200

    Fix title

--- src/Components/Content/header-template.jsx
+++ src/Components/Content/header-template.jsx
@@ -76,7 +76,7 @@ export default (props) => {
     return (
         <Router>
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                <Link to="/" className="navbar-brand" >Emprede fruits</Link>
+                <Link to="/" className="navbar-brand" >Emprende fruits</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
